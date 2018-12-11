62980648a3bf86ff62d4c7631154d7e67a867c7f
Joshua Duncan <41347949+JEduncan@users.noreply.github.com>
2018-12-10 20:34:34 -0500

    Add files via upload

---
+++ Online-library/inc/data.php
@@ -0,0 +1,171 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Great gatsby",
+	"img" => "img/media/greatgatsby.jpg",
+    "genre" => "Historical Fiction",
+    "format" => "Paperback",
+    "year" => 1925,
+    "category" => "Books",
+    "authors" => [
+        "F. Scott Fitzgerald",
+
+    ],
+    "publisher" => "Charles Scribner's Sons",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+    "title" => "Harry potter",
+    "img" => "img/media/harry.jpg",
+    "genre" => "drama,Fantasy,Young adult,Mystery,Thriller",
+    "format" => "Hardcover",
+    "year" => 1998,
+    "category" => "Books",
+    "authors" => [
+        "J. K. Rowling"
+    ],
+    "publisher" => "Bloomsbury",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "The Chronicles of Narnia ",
+    "img" => "img/media/Narnia.jpg",
+    "genre" => "Fantasy,Children's literature",
+    "format" => "Paperback",
+    "year" => 1950,
+    "category" => "Books",
+    "authors" => [
+        "C. S. Lewis",
+
+    ],
+    "publisher" => "The Bodley Head ‎Geoffrey Bles",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "Holy bible",
+    "img" => "img/media/bible.jpg",
+    "genre" => "History",
+    "format" => "Paperback",
+    "year" => "6th century B.C",
+    "category" => "Books",
+    "authors" => [
+        "God"
+    ],
+
+];
+//Movies
+$catalog[201] = [
+    "title" => "Avengers",
+    "img" => "img/media/avengers.jpg",
+    "genre" => "Action,Adventure,Fantasy",
+    "format" => "DVD",
+    "year" => 2012,
+    "category" => "Movies",
+    "director" => "Joss Whedon","Anthony Russo","Joe Russo",
+    "writers" => [
+        "Joss Whedon",
+
+    ],
+    "stars" => [
+        "Robert Downey Jr",
+        "Chris Evans",
+        "Mark Ruffalo",
+        "Chris Hemsworth",
+				"Scarlett Johansson",
+				"Jeremy Renner"
+    ]
+];
+$catalog[202] = [
+    "title" => "Black panther",
+    "img" => "img/media/blackpanther_main.jpg",
+    "genre" => "Action,Adventure,Fantasy",
+    "format" => "Blu-ray",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Ryan Coogler",
+    "writers" => [
+        "Ryan Coogler"
+    ],
+    "stars" => [
+        "Chadwick Boseman",
+        "Michael B.Jordan",
+        "Lupita Nyong'o",
+        "Danai Gurira",
+
+    ]
+];
+$catalog[203] = [
+    "title" => "Spiderman",
+    "img" => "img/media/spiderman home.jpg",
+    "genre" => "Action,Adventure",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Jon Watts",
+    "writers" => [
+        "Jonathan Goldstein",
+
+    ],
+    "stars" => [
+        "Tom Holland",
+        "Michael Keaton",
+        "Robert Downey Jr.",
+
+    ]
+];
+$catalog[204] = [
+    "title" => "Warriors",
+    "img" => "img/media/warriors.jpg",
+    "genre" => "Action,Crime,Thriller",
+    "format" => "DVD",
+    "year" => 1979,
+    "category" => "Movies",
+    "director" => "Walter Hill",
+    "writers" => [
+        " Sol Yurick"
+    ],
+    "stars" => [
+        "Michael Beck",
+        "James Remar",
+        "Dorsey Wright",
+
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "24K Magic",
+    "img" => "img/media/br.jpg",
+    "genre" => "Contemporary R&B, Funk, Pop music",
+    "format" => "CD,Digital",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Bruno Mars"
+];
+$catalog[302] = [
+    "title" => "Coloring Book",
+    "img" => "img/media/chance.jpg",
+    "genre" => " ‎Hip hop‎; ‎gospel rap",
+    "format" => "CD,Digital",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Chance The Rapper"
+];
+$catalog[303] = [
+    "title" => "Damn",
+    "img" => "img/media/kendricklamar.jpg",
+    "genre" => "Hip-Hop/Rap",
+    "format" => "CD,Digital",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Kendrick Lamar"
+];
+$catalog[304] = [
+    "title" => "YSIV",
+    "img" => "img/media/logicyoung.jpg",
+    "genre" => "Hip-Hop/Rap",
+    "format" => "CD,Digital",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Nat King Cole"
+];
